69fa4ca2c09e59c1bba60fe9a609d4f5615604ba
Martin gmail <martinkrecek9@gmail.com>
2023-04-29 13:59:15 +0200

    update backup for vehiclepositions

--- sql/vehiclepositions/100_backup.sql
+++ sql/vehiclepositions/100_backup.sql
@@ -2,5 +2,27 @@ TRUNCATE stg_vehiclepositions_backup;
 
 INSERT INTO stg_vehiclepositions_backup
 SELECT
-    *
+    vehicleposition_id,
+    geometry_coordinates,
+    gtfs_trip_id,
+    gtfs_route_id,
+    gtfs_route_type,
+    gtfs_trip_headsign,
+    gtfs_trip_short_name,
+    gtfs_route_short_name,
+    agency_name_real,
+    agency_name_scheduled,
+    sequence_id,
+    start_timestamp,
+    last_stop_arrival,
+    last_stop_departure,
+    last_stop_id,
+    last_stop_sequence,
+    last_stop_arrival_time,
+    next_stop_id,
+    next_stop_sequence,
+    next_stop_arrival_time,
+    next_stop_departure_time,
+    is_canceled,
+    origin_timestamp
 FROM stg_vehiclepositions;
