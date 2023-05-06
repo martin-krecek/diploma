1f0cd0887b2adca5e3d47f50a952c4c6709bebb2
Martin gmail <martinkrecek9@gmail.com>
2023-05-06 21:42:01 +0200

    Add output stage SQL + DAG

---
+++ dags/output_stage.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.mysql_operator import MySqlOperator
+from datetime import datetime
+
+def_conn_id = "mysql-db"
+
+# Define the DAG
+dag = DAG(
+    dag_id=output_stage,
+    start_date=datetime(2023, 3, 12),
+    schedule_interval=None,
+    catchup=False,
+    template_searchpath=["/home/melicharovykrecek/diploma/sql"]
+)
+
+create_output_stage = MySqlOperator(
+    sql=f'/output_stage.sql',
+    task_id=f"output_stage",
+    mysql_conn_id=def_conn_id,
+)
+
+# Set task dependencies
+create_output_stage
---
+++ sql/output_stage.sql
@@ -0,0 +1,86 @@
+-- out_parking_measurements
+CREATE OR REPLACE VIEW `out_parking_measurements` AS 
+select 
+  `stg_parking_measurements`.`parking_measurement_id` AS `parking_measurement_id`, 
+  `stg_parking_measurements`.`source` AS `source`, 
+  `stg_parking_measurements`.`source_id` AS `source_id`, 
+  `stg_parking_measurements`.`parking_id` AS `parking_id`, 
+  `stg_parking_measurements`.`date_modified` AS `date_modified`, 
+  `stg_parking_measurements`.`total_spot_number` AS `total_spot_number`, 
+  `stg_parking_measurements`.`occupied_spot_number` AS `occupied_spot_number`, 
+  `stg_parking_measurements`.`available_spot_number` AS `available_spot_number`,
+  'stg' AS `_sys_source_table` 
+from 
+  `stg_parking_measurements` 
+where 
+  (
+    `stg_parking_measurements`.`date_modified` > '2023-03-18'
+  ) 
+union 
+select 
+  `stg_parking_measurements_history`.`parking_measurement_id` AS `parking_measurement_id`, 
+  `stg_parking_measurements_history`.`source` AS `source`, 
+  `stg_parking_measurements_history`.`source_id` AS `source_id`, 
+  `stg_parking_measurements_history`.`parking_id` AS `parking_id`, 
+  `stg_parking_measurements_history`.`date_modified` AS `date_modified`, 
+  `stg_parking_measurements_history`.`total_spot_number` AS `total_spot_number`, 
+  `stg_parking_measurements_history`.`occupied_spot_number` AS `occupied_spot_number`, 
+  `stg_parking_measurements_history`.`available_spot_number` AS `available_spot_number`,
+  'stg_history' AS `_sys_source_table` 
+from 
+  `stg_parking_measurements_history` 
+where 
+  (
+    `stg_parking_measurements_history`.`date_modified` < '2023-03-18'
+  );
+
+-- out_weather
+CREATE OR REPLACE VIEW `out_weather` AS 
+select 
+  `stg_weather_forecast`.`weather_forecast_id` AS `weather_id`, 
+  `stg_weather_forecast`.`time_ts` AS `time_ts`, 
+  `stg_weather_forecast`.`time` AS `time`, 
+  `stg_weather_forecast`.`precipitation` AS `precipitation`, 
+  `stg_weather_forecast`.`temperature` AS `temperature`, 
+  `stg_weather_forecast`.`snowfall` AS `snowfall`, 
+  `stg_weather_forecast`.`rain` AS `rain`,
+  'stg_forecast' AS `_sys_source_table` 
+from 
+  `stg_weather_forecast` 
+where 
+  (
+    `stg_weather_forecast`.`time_ts` > '2023-04-30'
+  ) 
+union 
+select 
+  `stg_weather_archive`.`weather_archive_id` AS `weather_id`, 
+  `stg_weather_archive`.`time_ts` AS `time_ts`, 
+  `stg_weather_archive`.`time` AS `time`, 
+  `stg_weather_archive`.`precipitation` AS `precipitation`, 
+  `stg_weather_archive`.`temperature` AS `temperature`, 
+  `stg_weather_archive`.`snowfall` AS `snowfall`, 
+  `stg_weather_archive`.`rain` AS `rain`,
+  'stg_archive' AS `_sys_source_table` 
+from 
+  `stg_weather_archive` 
+where 
+  (
+    `stg_weather_archive`.`time_ts` <= '2023-04-30'
+  )
+  ORDER BY time_ts DESC;
+
+-- out_vehiclepositions
+CREATE OR REPLACE VIEW `out_vehiclepositions` AS 
+select 
+  `stg_vehiclepositions`.`vehicleposition_id` AS `vehicleposition_id`,
+  `stg_vehiclepositions`.`geometry_coordinates` AS `geometry_coordinates`,
+  `stg_vehiclepositions`.`gtfs_trip_id` AS `gtfs_trip_id`,
+  `stg_vehiclepositions`.`gtfs_route_id` AS `gtfs_route_id`,
+  `stg_vehiclepositions`.`gtfs_route_type` AS `gtfs_route_type`,
+  `stg_vehiclepositions`.`start_timestamp` AS `start_timestamp`,
+  `stg_vehiclepositions`.`actual` AS `actual`,
+  `stg_vehiclepositions`.`last_stop_sequence` AS `last_stop_sequence`,
+  `stg_vehiclepositions`.`last_stop_arrival_time` AS `last_stop_arrival_time`,
+  'stg' AS `_sys_source_table`
+from 
+  `stg_vehiclepositions`;
